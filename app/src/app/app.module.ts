5ff92d8af89ec6000551f3011b461c5ef0de7a04
¨ProjectD20 <projectdev0220@gmail.com>
2020-03-31 12:15:37 +0100

    set envirement dependencies

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import firebaseConfig from './firebase';
+import _AngularFireModule from '@angular/fire';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
