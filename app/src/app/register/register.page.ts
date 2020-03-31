fea365d34a16d65bf15f0adfb07a89361bda3134
¨ProjectD20 <projectdev0220@gmail.com>
2020-03-31 22:01:05 +0100

    set register page

--- app/src/app/register/register.page.ts
+++ app/src/app/register/register.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AngularFireAuth} from '@angular/fire/auth';
+import {auth} from 'firebase/app';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+username :string ="";
+password : string="";
+cpassword :string="";
+  constructor(public afAuth:AngularFireAuth) { }
 
   ngOnInit() {
   }
-
+  async register(){
+  const{username,password,cpassword}=this
+  if(password!=cpassword)
+  {
+    console.error("passwords don't match");
+}
+try{
+const res=await this.afAuth.auth.createUserWithEmailAndPassword(username+"@socialm.com",password);
+console.log(res);
+}
+catch(error){
+console.dir(error);
+}
+}
 }
