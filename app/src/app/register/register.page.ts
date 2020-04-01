6e2ee4c3dab4432b66e0859bf944e65104489104
¨ProjectD20 <projectdev0220@gmail.com>
2020-04-01 12:19:28 +0100

    modify alert call

--- app/src/app/register/register.page.ts
+++ app/src/app/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {auth} from 'firebase/app';
+import {AlertController} from "@ionic/angular" ;
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -10,7 +11,8 @@ export class RegisterPage implements OnInit {
 username :string ="";
 password : string="";
 cpassword :string="";
-  constructor(public afAuth:AngularFireAuth) { }
+  constructor(public afAuth:AngularFireAuth,
+    public alertCont:AlertController) { }
 
   ngOnInit() {
   }
@@ -18,7 +20,10 @@ cpassword :string="";
   const{username,password,cpassword}=this
   if(password!=cpassword)
   {
+    this.showAlert("Error","Passwords don't match");
     console.error("passwords don't match");
+    return 0;
+  
 }
 try{
 const res=await this.afAuth.auth.createUserWithEmailAndPassword(username+"@socialm.com",password);
@@ -27,5 +32,15 @@ console.log(res);
 catch(error){
 console.dir(error);
 }
+}
+async showAlert(header:string, message:string){
+  console.log("enter show alert");
+const alert=await this.alertCont.create(
+{
+  header,message,buttons:["ok"]
+});
+console.log("after create alert call");
+await alert.present();
+ console.log("after await alert test preesence");
 }
 }
